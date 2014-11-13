a4d4d195cce3d86e254c6097f2609b08952572fd
dj80hd <werwath@gmail.com>
2014-11-13 16:29:35 -0600

    fixed comments

--- popup.js
+++ popup.js
@@ -1,12 +1,10 @@
 //FIXME - How do we avoid the global namespace?
-//FIXME - How do we properly establish the dependency on week.js
 //
 /**
- * Given a photo, construct a URL using the method outlined at
- * http://www.flickr.com/services/api/misc.urlKittenl
+ * Appends the week number to the document body
  *
- * @param {DOMElement} A kitten.
- * @return {string} The kitten's URL.
+ * @param
+ * @return
  * @private
  */
 function show_me_the_week_number() {
